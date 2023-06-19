3c89e50c61580a3ef12e24abf98402f283fc538f
devpthunder <devpthunder@gmail.com>
2023-06-19 15:00:53 +0300

    Patience Task commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+		putchar(48);
+		putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
