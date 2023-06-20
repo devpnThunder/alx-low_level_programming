b37cf73a00ad037616d6663841a60bd1deb1d85f
devpThunder <devpthunder@gmail.com>
2023-06-21 02:20:16 +0300

    isalpha Task commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _isalpha - checks for alphabetic characters
+* @c: prints the character
+*
+*Return: 1 for letter or 0 otherwise
+*/
+int _isalpha(int c)
+{
+	if (c >= 65 && <= 90)
+	{
+		if (c >= 97 && c <= 122)
+		{
+			return (1);
+		}
+	}
+	else
+	{
+		return (0);
+	}
+}
