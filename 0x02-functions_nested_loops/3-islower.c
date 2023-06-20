7c2495c00b877e49a7a4c2f5904d819eba8ceda2
devpThunder <devpthunder@gmail.com>
2023-06-21 02:01:20 +0300

    islower Task commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _islower - checks for lowercase character
+* @c: character to print
+*
+* Return: lowercase 1 otherwise 0
+*/
+int _islower(int c)
+{
+	if (c = islower(c))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
