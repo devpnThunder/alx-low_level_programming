a212ef4d0eacfddcdd941ece30a7d0bd5b3d5d49
devpThunder <devpthunder@gmail.com>
2023-06-21 19:50:04 +0300

    Learn your times table Task commit #2

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,4 +7,24 @@
 */
 void times_tables(void)
 {
+	int a, b, m;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			m = a * b;
+			if (b != 57)
+			{
+				_putchar(m);
+				_putchar(44);
+				_putchar(32);
+			}
+			else
+			{
+				_putchar(m);
+			}
+		}
+		_putchar('\n');
+	}
 }
