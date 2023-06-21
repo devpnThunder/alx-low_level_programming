1ac94ac523bc0bbd26fe6b3dd28bb8db178aa6d4
devpThunder <devpthunder@gmail.com>
2023-06-22 00:16:14 +0300

    Fibonacci Task commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n = 50;
+	int a = 1, b = 2, c;
+
+	printf("%d, %d", a, b);
+	for (int i = 2; i < n; i++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		printf("%d, ", c);
+	}
+	return (0);
+}
