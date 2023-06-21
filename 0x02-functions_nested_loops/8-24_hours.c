98d38de9902d184e3c15e21dc72c089c1130eabf
devpThunder <devpthunder@gmail.com>
2023-06-21 07:04:00 +0300

    Federal Agent Task commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* jack_bauer - prints every minute of the day
+*
+* Return: zero unless otherwise stated
+*/
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a <= 50; a++)
+	{
+		for (b = 48; b <= 51; b++)
+		{
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
