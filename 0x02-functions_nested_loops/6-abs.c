a9011f58b873730086926ce568f0a020f2c30d71
devpThunder <devpthunder@gmail.com>
2023-06-21 06:25:06 +0300

    Absolute value in this world Task commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _abs - computes absolute value of an integer
+* @i: absolute value
+*
+* Return: if successful 0
+*/
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
