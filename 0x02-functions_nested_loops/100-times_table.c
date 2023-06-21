ec8bd0d6089bcaa1ca76217c0b499567c6f1689a
devpThunder <devpthunder@gmail.com>
2023-06-21 23:43:42 +0300

    Multiplication table Task commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* print_times_table - prints n times table
+* @n: integer number
+*
+* Return: times table
+*/
+void print_times_table(int n)
+{
+	int i, j, mult;
+
+if (n >= 0 && n <= 15)
+{
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(48);
+		for (j = 1; j <= n; j++)
+		{
+			mult = i * j;
+			_putchar(44);
+			_putchar(32);
+			if (mult <= 9)
+			{
+				_putchar(32);
+				_putchar(32);
+				_putchar(mult + 48);
+			}
+			else if (mult <= 99)
+			{
+				_putchar(32);
+				_putchar((mult / 10) + 48);
+				_putchar((mult % 10) + 48);
+			}
+			else
+			{
+				_putchar(((mult / 100) % 10) + 48);
+				_putchar(((mult / 10) % 10) + 48);
+				_putchar((mult % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
+}
