3f3cc5bbe7e0931049487bfaa254bd299dc5633f
devpThunder <devpthunder@gmail.com>
2023-06-24 00:02:06 +0300

    Numbers constitute Task commit #6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,24 @@
 #include "main.h"
 
 /**
-* more_numbers - prints 10 times numbers from 0 to 14
-*
-* Return: Always 0 unless otherwise
-*/
+ * more_numbers - prints numbers between 0 to 14
+ * 10 times.
+ * Return: no return.
+ */
+
 void more_numbers(void)
 {
 	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j);
+			if (j >= 10)
+			{
+				_putchar((j / 10) + 48);
+			}
+			_putchar((j % 10) + 48);
 		}
 		_putchar('\n');
 	}
