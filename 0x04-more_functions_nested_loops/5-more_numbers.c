91cf9ca9abfd285937e4c710860157847a70d899
devpThunder <devpthunder@gmail.com>
2023-06-23 23:48:28 +0300

    Numbers constitute Task commit #4

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j);
+			if (j >= 10)
+			{
+				_putchar((j / 10) + 48);
+			}
+			_putchar((j % 10) + 48);
 		}
 		_putchar('\n');
 	}
