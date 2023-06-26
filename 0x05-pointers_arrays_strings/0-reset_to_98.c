802a71fa208e78db602a203d45c7666f33990488
devpThunder <devpthunder@gmail.com>
2023-06-27 02:21:06 +0300

    98 Battery st Task commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* reset_to_98 - takes a pointer and updates the value it points to
+* @*n: pointer to an integer
+*
+* Return: Always 0 unless otherwise
+*/
+void reset_to_98(int *n)
+{
+	int *p;
+
+	p = *n;
+	*p = 98;
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Pointers arrays strings directory readme file
