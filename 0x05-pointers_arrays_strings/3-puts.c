617f867b81194a9b7d4e599a00536862286728b6
devpThunder <devpthunder@gmail.com>
2023-06-27 03:51:05 +0300

    I do not fear computer Task commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _puts - function that prints a string
+* @str: string characters
+*
+* Return: the string characters
+*/
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (str[i] != '\0')
+		{
+			_putchar(str[i]);
+		}
+	i++;
+	_putchar('\n');
+	}
+}
