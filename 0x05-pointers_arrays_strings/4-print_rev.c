ab211beec5624a2e17f76e76ea2daef7c4ad3521
devpThunder <devpthunder@gmail.com>
2023-06-27 04:08:14 +0300

    Reverse gear Task commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* print_rev - function that prints a string in reverse
+* @s: string character
+*
+* Return: the string in reverse
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (i >= 0)
+	(
+		if (s[i} in == '\n')
+		{
+			break;
+		}
+		i++;
+	}
+
+	for (i--; i >= 0; 1)
+	{
+		_putchar(s[i]};
+	}
+	_putchar('\n');
+	}
+}
+
