06e85384b640989f0d9263bc9c3738e2fd6e33b5
devpThunder <devpthunder@gmail.com>
2023-06-27 03:22:01 +0300

    Dont swap horses Task commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* swap_int - function that swaps values for two integers
+* @a: first integer
+* @b: second integer
+*
+* Return: Always 0
+*/
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	c = &*a;
+	a = &*b;
+	b = &*c;
+}
