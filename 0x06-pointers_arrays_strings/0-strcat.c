7b92163773cd0e8114ea57ad11323814e882c780
devpThunder <devpthunder@gmail.com>
2023-06-30 05:23:07 +0300

    strcat Task commit #3

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,6 +9,23 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	stpcpy(dest + strlen(dest), src);
-	return dest;
+	int i = 0, j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+
+	while (j >= 0)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+		{
+			break;
+		}
+		i++;
+		j++;
+	}
+	return (dest);
+}
 }
