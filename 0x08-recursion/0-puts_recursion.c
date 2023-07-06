9eabe12ce620a64e81b773b7cba9e2dc2ea6de79
devpnThunder <devpthunder@gmail.com>
2023-07-06 09:44:34 +0300

    She locked away a secret Task commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _puts_recursion - prints a string
+* @s: character string
+*
+* Return: string and a new line
+*/
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+	_putchar('\n');
+}
