8bd5398f5a3da895f365475832fedf27f99de0a7
devpnThunder <devpthunder@gmail.com>
2023-07-06 16:46:28 +0300

    Why is it important Task commit #2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,11 +10,11 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar(*s);
-		_print_rev_recursion(s - 1);
+		_putchar('\n');
 	}
 	else
 	{
-		_putchar('\n');
+		_putchar(*s);
+		_print_rev_recursion(s - 1);
 	}
 }
