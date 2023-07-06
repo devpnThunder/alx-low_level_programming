fe8d3977cc07a44c87cde825666727ee1d2f8eec
devpnThunder <devpthunder@gmail.com>
2023-07-06 18:59:23 +0300

    Power Task commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _pow_recursion - function that returns the value of x raised to power y
+* @x: first integer value
+* @y: second integer value
+*
+* Return: value of x raised to power y
+*/
+int _pow_recursion(int x, int y)
+{
+	int pwr;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (x ** y);
+}
