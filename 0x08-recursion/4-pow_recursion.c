220d0a542f18434de08bdf95928259658c6b8c5e
devpnThunder <devpthunder@gmail.com>
2023-07-06 19:41:40 +0300

    Power Task commit #5

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,6 +15,12 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	pwr = x ^ y;
-	return (pwr);
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
 }
