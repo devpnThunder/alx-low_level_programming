cd33d2d6ae4a00e3946b6178f0ca4eb5f681a3f5
devpnThunder <devpthunder@gmail.com>
2023-07-08 06:53:02 +0300

    Static library Task commit

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _isupper - function that prints upper case characters
+* @c: integer character
+*
+* Return: return c character
+*/
+int _isupper(int c)
+{
+	return (0);
+}
