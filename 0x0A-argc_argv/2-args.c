f5dfcbdceca9b10778d6854a1abc1868516a0051
devpnThunder <devpthunder@gmail.com>
2023-07-11 10:59:25 +0300

    The best argument against democracy Task commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* @argc: integer character
+* @argv: a pointer to a character string
+*
+* Return: Always 0 unless otherwise
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%d\n", argv[i]);
+	}
+	return (0);
+}
