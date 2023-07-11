b09074b7d56aa06a92097ae88dca8f583ef98525
devpnThunder <devpthunder@gmail.com>
2023-07-11 04:36:34 +0300

    The best argument Task commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* @argc: integer character
+* @argv: pointer to string character
+*
+* Return: Always 0 (successful)
+*/
+int main(int argc, char *argv[])
+{
+	_putchar(argc);
+	return (0);
+}
