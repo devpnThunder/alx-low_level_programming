448e4b13e8c81b391f0a7e1495e3d4174ec2ce3f
devpnThunder <devpthunder@gmail.com>
2023-07-11 10:41:37 +0300

    Silence is argument Task commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* @argc: integer character
+* @argv: pointer to a character string
+*
+* Return: Always 0 unless otherwise
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
