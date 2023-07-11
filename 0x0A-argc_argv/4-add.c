13b835ad3a9b3edd8739f09a816c3cad2ea11df0
devpnThunder <devpthunder@gmail.com>
2023-07-11 14:00:09 +0300

    To infinity and beyond Task commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* main - entry point
+* @argc: integer variable
+* @argv: pointer character variable
+*
+* Return: return 0 if Successful
+*/
+
+int main(int argc, char *argv[])
+{
+	int m, n, add = 0;
+
+	for (m = 1; m < argc; m++)
+	{
+		for (n = 0; argv[m][n] != '\0'; n++)
+		{
+			if (!isdigit(argv[m][n]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[m]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
