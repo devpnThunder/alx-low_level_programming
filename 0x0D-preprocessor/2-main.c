a1c846cc6f7e7b1836f8f2b8bd178235cf06d807
devpnThunder <devpthunder@gmail.com>
2023-07-18 00:38:48 +0300

    File Name Task commit #3

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,15 @@
 #ifndef __FILE__
 #define __FILE__
 
-int main()
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: the name of the source code file
+*/
+
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
