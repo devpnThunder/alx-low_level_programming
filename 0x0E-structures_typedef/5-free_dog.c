be90cd769d96aa5205da3d7fe45a41c0a8c7add9
devpnThunder <devpthunder@gmail.com>
2023-07-18 05:59:16 +0300

    Free Dog Task commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+/**
+* free_dog - function that frees dogs
+* @name: pointer to a character variable name
+* @age: float variable age
+* @owner: pointer to a character variable owner
+*
+* Return: new dog otherwise null
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *a_dog;
+
+	a_dog = malloc(sizeof(dog_t));
+	if (a_dog == NULL)
+		return (NULL);
+	a_dog->name = name;
+	a_dog->age = age;
+	a_dog->owner = owner;
+	return (a_dog);
+	free(a_dog);
+}
