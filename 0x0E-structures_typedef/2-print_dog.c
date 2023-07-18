42f866a3a3144b05311c0fb9c7b3ef1a13bd1b7a
devpnThunder <devpthunder@gmail.com>
2023-07-18 05:12:49 +0300

    A dog will teach you unconditional love Task commit #2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,29 +9,29 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d.name == NULL)
+		if (d->name == NULL)
 		{
-			printf("Name: (nil)");
-			printf("Age: %d", d.age);
-			printf("Owner: %s", d.owner);
+			printf("Name: (nil)\n");
+			printf("Age: %f\n", d->age);
+			printf("Owner: %s\n", d->owner);
 		}
-		else if (d.age == NULL)
+		else if (d->age == NULL)
 		{
-			printf("Name: %s", d.name);
-			printf("Age: (nil)");
-			printf("Owner: %s", d.owner);
+			printf("Name: %s\n", d->name);
+			printf("Age: (nil)\n");
+			printf("Owner: %s\n", d->owner);
 		}
-		else if (d.owner == NULL)
+		else if (d->owner == NULL)
 		{
-			printf("Name: %s", d.name);
-			printf("Age: %d", d.age);
-			printf("Owner: (nil)");
+			printf("Name: %s\n", d->name);
+			printf("Age: %f\n", d->age);
+			printf("Owner: (nil)\n");
 		}
 		else
 		{
-			printf("Name: %s", d.name);
-			printf("age: %d", d.name);
-			printf("Owner: %s", d.owner);
+			printf("Name: %s\n", d->name);
+			printf("age: %f", d->name);
+			printf("Owner: %s\n", d->owner);
 		}
 	}
 }
