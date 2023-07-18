de26892393bf7758edc9a1d748a68a500f179ac4
devpnThunder <devpthunder@gmail.com>
2023-07-18 03:42:41 +0300

    Poppy Task commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+* struct dog - Structure of a dog
+* @name: character variable name
+* @age: float variable age
+* @owner: character variable owner
+*
+* Description: A structure of a dog with name, age and owner variables
+*/
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
