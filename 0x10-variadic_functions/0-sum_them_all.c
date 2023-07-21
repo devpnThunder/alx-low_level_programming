40dcecefb7c5f83ac93b204eb43328f54040e4c7
devpnThunder <devpthunder@gmail.com>
2023-07-21 04:28:12 +0300

    Beauty is variable Task commit #3

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,22 +8,21 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
-	int i, sum;
+	va_list args;
+	int i, sum = 0;
 
-	va_start(ap, n);
-	sum = 0;
-	if (n == 0)
+	va_start(args, n);
+	for (i = 0; i < n; i++)
 	{
-		return (0);
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
+		if (n == 0)
 		{
-			sum += va_arg(ap, int);
+			return (0);
 		}
-		va_end(ap);
-		return (sum);
+		else
+		{
+			sum += va_arg(args, int);
+		}
 	}
+	va_end(args);
+	return (sum);
 }
