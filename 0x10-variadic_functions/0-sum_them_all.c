337e6a083361e23e8d8dbec11c683d94ed3e70ef
devpnThunder <devpthunder@gmail.com>
2023-07-21 04:14:42 +0300

    Beauty is variable Task commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+* sum_them_all - function that returns the sum of all its parameters
+* @n: integer character
+*
+* Return: 0 if n == 0
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i, sum;
+
+	va_start(ap, n);
+	sum = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			sum += va_arg(ap, int):
+		}
+		va_end(ap);
+		return (sum);
+	}
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_string(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VARIADIC_FUNCTIONS */
