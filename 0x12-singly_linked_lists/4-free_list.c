fc9bffc3cccf7521f6137b2dfdad4543ddceb1d7
devpnThunder <devpthunder@gmail.com>
2023-07-28 05:31:36 +0300

    Free list Task commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* free_list - free a `list_t` list
+* @head: head of linked list
+*
+* Return: no return
+*/
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
