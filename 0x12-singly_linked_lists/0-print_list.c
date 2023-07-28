f6daeba772c8b1b0e59a7bd57040a4a4e59a130b
devpnThunder <devpthunder@gmail.com>
2023-07-28 05:50:29 +0300

    Print list Task commit #4

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t c;
-
-	c = 0;
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		c++;
-	}
-	return (c);
+	if (h->str)
+		printf("[%d] %s\n", h->len, h-str);
+	else
+		printf("[%d] %s\n", 0, "(nil)")';
+	return (h->next);
 }
